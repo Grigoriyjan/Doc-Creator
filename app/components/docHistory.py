4a977afe8239b845f6cc90790569ad5acf111ee3
Miku <strarti@bk.ru>
2024-10-21 21:53:55 +0600

    оптимизация

--- app/components/docHistory.py
+++ app/components/docHistory.py
@@ -1,5 +1,4 @@
 import json
-import tkinter as tk
 from tkinter import ttk
 
 doc_types = ['Счет на ОПЛ', 'КП']
@@ -8,21 +7,23 @@ props = ["ИП Алапаев","Эмин и Ко"]
 def draw_header(root):
     top_titles = ['Реквизиты','Документ', 'Накладная', 'Имя', 'сумма', 'дата']
     for index, title in enumerate(top_titles):
-        label = ttk.Label(root, text=title)
-        label.grid(row = 2, column= index, padx=60, pady=15)
+        ttk.Label(root, text=title).grid(row = 2, column= index, padx=60, pady=15)
 
 def drawPastDocs(root):
-    with open('app\data\JSON\docHistory.json','r', encoding="utf-8") as file:
+    with open('app\data\JSON\docHistory.json', 'r', encoding="utf-8") as file:
         doc_list = json.load(file)['docs']
-    
-    draw_header(root)
-    
-    for i in range(len(doc_list)):
-        common = doc_list[i][len(doc_list[i]) - 1]
-        ttk.Label(root, text=doc_types[common['docType']]).grid(row=i + 3, column=1, padx=30, pady=15)
-        ttk.Label(root, text=props[doc_list[i][0]]).grid(row=i + 3, column=0, padx=30, pady=15)
-        for j in range(3):
-            ttk.Label(root, text=doc_list[i][j + 1]).grid(row=i + 3, column=j + 2, padx=70, pady=15)
-        date = common['date']
-        ttk.Label(root, text=f"{date['currDay']}.{date['currMoth']}.{date['currYear']}").grid(row=i + 3, column=j + 3, padx=70, pady=15)
+        draw_header(root)
+        
+        for i, doc in enumerate(doc_list):
+            common = doc[-1]
+            date = common['date']
+            
+            labels = [
+                (doc_types[common['docType']], 1),
+                (props[doc[0]], 0),
+                (f"{date['currDay']}.{date['currMoth']}.{date['currYear']}", 5)
+            ] + [(doc[j + 1], j + 2) for j in range(3)]
+            print(labels)
+            for text, col in labels:
+                ttk.Label(root, text=text).grid(row=i + 3, column=col, padx=30, pady=15)
     
